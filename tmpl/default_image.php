a4ce8f6eb92be00d9802c3b179bbb0a60963fb4e
hoochicken <mareike.riegel@ql.de>
2020-04-16 13:30:38 +0200

    link images implemented

--- tmpl/default_image.php
+++ tmpl/default_image.php
@@ -7,16 +7,17 @@
  */
 defined('_JEXEC') or die;
 
+/** @var stdClass $params */
 /** @var stdClass $objJson */
 /** @var string $strFieldName */
 $strCaption = !empty($objJson->image_intro_caption) ? $objJson->{$strFieldName . '_caption'} : $item->title;
 $strSource = $objJson->$strFieldName;
 $strAlt = $objJson->{$strFieldName . '_alt'};
 ?>
-<?php if (1 == $params->get('link_images')) : ?>
+<?php if (1 == $params->get('linkImages')) : ?>
     <a href="<?php echo $item->link;?>">
 <?php endif; ?>
     <img src="<?php echo $strSource; ?>" alt="<?php echo $strAlt;?>" title="<?php echo $strCaption; ?>" />
-<?php if (1 == $params->get('link_images')) : ?>
+<?php if (1 == $params->get('linkImages')) : ?>
 </a>
 <?php endif; ?>
